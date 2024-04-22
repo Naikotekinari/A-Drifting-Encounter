5b45102ddbefb9ccada4fafef0d9c001bc8c5c22
Naikotekinari <163132808+Naikotekinari@users.noreply.github.com>
2024-04-22 15:23:49 +0100

    the beginning

---
+++ areaTables.js
@@ -0,0 +1,28 @@
+export const areaTables = [
+    {
+        name: "Erimera (Desert)",
+        encounterDice: 20,
+        encounters: [
+            {type: "scourge", 
+            img: "scourge.png", 
+            mod: "n",
+            long: false,
+            exArr: "n"},
+            {type: "scourge",
+            img: "scourge.png",
+            long: false,
+            mod:"n",
+            exArr: "n"},
+            {type: "empty",
+            img: "empty.png",
+            long: false,
+            mod: "n"
+            
+            }
+
+            
+        ]
+    }
+]
+
+//encounter array defining: type, image, modifiers, extra array
---
+++ encounter/encounter.js
@@ -0,0 +1,14 @@
+let encounterCount = 0
+let explorationXP = 0
+let scourgePoints = 0
+let currentEncounter = ""
+
+window.onload = init;
+function init() {
+    const encounterCountText = document.getElementById("EncounterCount")
+    const explorationXPText = document.getElementById("ExplorationXP")
+    const scourgePointsText = document.getElementById("ScourgePoints")
+    const currentEncounterText = document.getElementById("CurrentEncounter")
+
+    
+}
