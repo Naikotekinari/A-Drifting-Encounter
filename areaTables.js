0b6ccbe9329d9d6a555a1c55a59d858ae5d2f576
Naikotekinari <163132808+Naikotekinari@users.noreply.github.com>
2024-04-23 14:27:35 +0100

    a LOT more

--- areaTables.js
+++ areaTables.js
@@ -1,28 +1,25 @@
-export const areaTables = [
+export let areaTables = { 
+    erimera: 
     {
         name: "Erimera (Desert)",
-        encounterDice: 20,
-        encounters: [
-            {type: "scourge", 
-            img: "scourge.png", 
-            mod: "n",
-            long: false,
-            exArr: "n"},
-            {type: "scourge",
-            img: "scourge.png",
-            long: false,
-            mod:"n",
-            exArr: "n"},
-            {type: "empty",
-            img: "empty.png",
-            long: false,
-            mod: "n"
-            
-            }
-
-            
-        ]
+        encounters: ["1 d20", "scourge", "scourge", "blank", "blank", "blank", "blank", "blank", "blank", "lowMat", "fish", "lowHunt", "lowHunt", "npc", "midMat", "wildMag", "landmark", "npc", "highHunt", "highMat"],
+        mats: {pl: ["Acai Fruit", "Wyrmsqil"],
+                me: ["Serpent Meat", "Scorpion Meat", "Lucsyn Meat"],
+                fi: ["Sandlin", "Razorwolfe", "Goldsick Carp"],
+                ot: ["Spirit Esscense", "Miracle Seed"],
+                mons: [["Serpent", "serpent.png", "me 0", 1], ["Scorpion", "scorpion.png", "me 1", 1], ["Lucsyn", "lucsyn.png", "me 2", 1], ["Spirit", "spirit.png", "ot 0", 1]], },
+        lowMat: ["1 d6", ["pl 0", "1"], ["pl 0", "2"], ["pl 0", "2"], ["pl 1", "1"], ["pl 1", "1 d2"], ["ot 1", "1"]],
+        midMat: ["1 d6", ["pl 0", "1 d4 - 1 1"], ["pl 0", "2 d4 - 1 2"], ["pl 1", "1 d2"], ["pl 1", "1 d2 + 1"], ["ot 1", "1"], ["ot 1", "1"]],
+        highMat: ["1 d6", ["pl 0", "1 d8 - 1 1"], ["pl 0", "2 d8 - 1 2"], ["pl 1", "1 d4"], ["pl 1", "1 d4 + 1"], ["ot 1", "1"], ["ot 1", "2"]],
+        lowHunt: ["1 d4", ["mons 0", "1 d4 - 1"], ["mons 1", "1 d4 - 1"], ["mons 2", "1"], ["mons 3", "1 d2"]],
+        highHunt: ["1 d4", ["mons 0", "1 d4 - 1"], ["mons 1", "1 d4 - 1"], ["mons 2", "1"], ["mons 3", "1 d2"]],
+        fish: ["1 d4", ["fi 0", "DC 12"], ["fi 0", "DC 12"], ["fi 1", "DC 14"], ["fi 2", "DC 18"]]
     }
-]
+}
 
 //encounter array defining: type, image, modifiers, extra array
+//matsEnc = plants, me = meat, fi = fish, ot = others
+//mons = Monster name, Monster image, drops, drop chance(1 = 100% 0.1 = 10%, etc), drops, drop chance, etc
+//mats: material name, "dice amount/flat number, dice type (ex. d20), modifiers, modifier numbers, minimum roll"
+//note for later, monsters appear, then press button after defeated, then loot appears
+// x in dice means roll several times
--- encounter/encounter.js
+++ encounter/encounter.js
@@ -1,7 +1,10 @@
+import { areaTables } from "/areaTables.js"
+
 let encounterCount = 0
 let explorationXP = 0
 let scourgePoints = 0
 let currentEncounter = ""
+let currentArea = "erimera"
 
 window.onload = init;
 function init() {
@@ -10,5 +13,166 @@ function init() {
     const scourgePointsText = document.getElementById("ScourgePoints")
     const currentEncounterText = document.getElementById("CurrentEncounter")
 
-    
-}
+    //rollDice("2 d100 + 4")
+    actionExplore()
+
+    function actionExplore() {
+        let encNum = rollDice(areaTables[currentArea].encounters[0])
+        let encoun = areaTables[currentArea].encounters[encNum]
+        encoun = "highMat"
+        switch(encoun) {
+            case "scourge":
+                console.log("the scourge is coming the scourge is coming the sc")
+                scourgePoints = scourgePoints + 1
+                break
+            case "blank":
+                console.log("Nothing's here, its blank...")
+                break
+            case "lowMat":
+                console.log("lowmat")
+                let lowMatRoll = rollDice(areaTables[currentArea].lowMat[0])
+                let lowMat = areaTables[currentArea].lowMat[lowMatRoll]
+                let lowMatItem = lowMat[0]
+                lowMatItem = findObject(lowMatItem)
+                console.log(`please work please work pl ${lowMatItem}`)
+                let lowMatNum = rollDice(lowMat[1])
+                console.log(`obtained ${lowMatNum} of ${lowMatItem}`)
+                break
+            case "midMat":
+                console.log("midmat")
+                let midMatRoll = rollDice(areaTables[currentArea].midMat[0])
+                let midMat = areaTables[currentArea].midMat[midMatRoll]
+                let midMatItem = midMat[0]
+                console.log(midMatItem)
+                midMatItem = findObject(midMatItem)
+                console.log(`please work please work pl ${midMatItem}`)
+                let midMatNum = rollDice(midMat[1])
+                console.log(`obtained ${midMatNum} of ${midMatItem}`)
+                break
+            case "highMat":
+                console.log("highmat")
+                let highMatRoll = rollDice(areaTables[currentArea].highMat[0])
+                let highMat = areaTables[currentArea].highMat[highMatRoll]
+                let highMatItem = highMat[0]
+                console.log(highMatItem)
+                highMatItem = findObject(highMatItem)
+                console.log(`please work please work pl ${highMatItem}`)
+                let highMatNum = rollDice(highMat[1])
+                console.log(`obtained ${highMatNum} of ${highMatItem}`)
+                break
+
+        }
+    }
+
+    function rollDice(dStr) {
+        const dice = dStr.split(" ")
+        console.log(dice)
+        if (dice.length < 2) {
+            return parseInt(dice[0])
+        }
+        let result = 0
+        
+        for (let i = 0; i < dice[0]; i++) {
+            switch(dice[1]){
+                case "d2":
+                    result = result + RNG(2)
+                    break;
+                case "d4": 
+                    result = result + RNG(4)
+                    break;
+                case "d6":
+                    result = result + RNG(6)
+                    break;
+                case "d8":
+                    result = result + RNG(8)
+                    break;
+                case "d10":
+                    result = result + RNG(10)
+                    break;
+                case "d12":
+                    result = result + RNG(12)
+                    break;
+                case "d20":
+                    result = result + RNG(20)
+                    break
+                case "d100":
+                    result = result + RNG(100)
+                    break
+                default:
+                    console.log("this isnt the right fice")
+                    break 
+            }
+        }
+        switch(dice[2]) {
+            case "+":
+                result = result + parseInt(dice[3])
+                break
+            case "-":
+                result = result - parseInt(dice[3])
+                break
+            case "*":
+                result = result * parseInt(dice[3])
+                break
+            case "/":
+                result = parseInt(result / parseInt(dice[3]))
+                break
+                
+        }
+        console.log(`result of dice is ${result}`)
+        return result
+    }
+
+    function findObject(obj) {
+        let objArr = obj.split(" ")
+        let objNum
+        let objItem
+        switch(objArr[0]) {
+            case ("pl"):
+                console.log("PL PLEASE")
+                objNum = parseInt(objArr[1])
+                console.log(objNum)
+                objItem = areaTables[currentArea].mats.pl
+                objItem = objItem[objNum]
+                return objItem
+            case ("me"):
+                console.log("ME PLEASE")
+                objNum = parseInt(objArr[1])
+                console.log(objNum)
+                objItem = areaTables[currentArea].mats.me
+                objItem = objItem[objNum]
+                return objItem
+            case ("fi"):
+                console.log("FI PLEASE")
+                objNum = parseInt(objArr[1])
+                console.log(objNum)
+                objItem = areaTables[currentArea].mats.fi
+                objItem = objItem[objNum]
+                
+                return objItem
+            case ("ot"):
+                console.log("OT PLEASE")
+                objNum = parseInt(objArr[1])
+                console.log(objNum)
+                objItem = areaTables[currentArea].mats.ot
+                objItem = objItem[objNum]
+                return objItem
+            case ("mons"):
+                console.log("MONS PLEASE")
+                objNum = parseInt(objArr[1])
+                console.log(objNum)
+                objItem = areaTables[currentArea].mats.mons
+                objItem = objItem[objNum]
+                return objItem
+                
+            
+        }
+    } 
+    }
+
+    function RNG(maxNum) {
+        let num = Math.floor(Math.random() * maxNum + 1);
+        console.log(`the number was ${num}`);
+        return num;
+    }
+
+
